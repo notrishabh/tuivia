5902991b102521546ee629ca0552be5a678b7e63
Rishabh <dplayarrr@gmail.com>
2025-01-07 02:08:04 +0530

    ADD: bubbletea package for full tui view. Moved huh to separate package.

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/charmbracelet/huh"
 	"github.com/joho/godotenv"
-	"github.com/notrishabh/tuivia/quiz"
+	"github.com/notrishabh/tuivia/tui"
 )
 
 func main() {
@@ -15,34 +13,5 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	var (
-		name     string
-		question string
-	)
-	var questions []quiz.QuizQuestion = quiz.Quiz()
-
-	form := huh.NewForm(
-		huh.NewGroup(
-			huh.NewSelect[string]().Title(questions[0].Question).Options(
-				huh.NewOption("Yushu boi", "yushu"),
-				huh.NewOption("ippi boi", "ippi"),
-				huh.NewOption("smol man", "smol"),
-			).Value(&name),
-			huh.NewSelect[string]().Value(&question).Title(questions[0].Question).OptionsFunc(func() []huh.Option[string] {
-				var opts []string
-				for _, v := range questions[0].Answers {
-					if v != "" {
-						opts = append(opts, v)
-					}
-				}
-				return huh.NewOptions(opts...)
-			}, &question),
-		),
-	)
-
-	err = form.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(name)
+	tui.RunTui()
 }
---
+++ tui/bubbletea.go
@@ -0,0 +1,81 @@
+package tui
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
+}
+
+func initialModel() model {
+	return model{
+		choices:  []string{"One", "two"},
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k", "w":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j", "s":
+			if m.cursor < len(m.choices) {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "\nWhat should we get?\n\n"
+
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+	s += "\nPress q to quit.\n"
+	return s
+}
+
+func RunTui() {
+	p := tea.NewProgram(initialModel())
+
+	_, err := p.Run()
+	if err != nil {
+		fmt.Printf("Error boi: %v", err)
+		os.Exit(1)
+	}
+}
